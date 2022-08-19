dc1a74729a07d04ad023a6ef368f9a225ea0b6c7
Custodio Serafim Langa <langacustodio@gmail.com>
2022-08-19 10:49:22 +0200

    Add components test

---
+++ src/test/Navbar.test.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import Renderer from 'react-test-renderer';
+import { BrowserRouter } from 'react-router-dom';
+import Navbar from '../Components/Navbar';
+
+function MockHeader() {
+  return (
+    <BrowserRouter>
+      <Navbar />
+    </BrowserRouter>
+  );
+}
+
+it('Renders correctly Navbar Component', () => {
+  const tree = Renderer
+    .create(<MockHeader />)
+    .toJSON();
+
+  expect(tree).toMatchSnapshot();
+});
